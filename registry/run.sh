3e03ee939020898406802f845451d07f4cbdbf03
Marco Martinez <mmmartinez@bluewin.ch>
2024-12-27 21:40:37 +0100

    first try manifest generator

--- registry/run.sh
+++ registry/run.sh
@@ -5,13 +5,15 @@ for arg in "$@"; do
   args+=("$arg")
 done
 
+manifest_generator="github.com/marcom4rtinez/terraform-registry-manifest/cmd/manifest@latest"
+
 search_string="\"version\": \"$2\","
 
 if grep -q "$search_string" registry-manifest.json; then
 echo "already up2date! Adding hashes now..."
-cat dist/*SHA256SUMS | go run registry/*.go --hashes --manifest registry-manifest.json
+cat dist/*SHA256SUMS | go run $manifest_generator  --hashes --manifest registry-manifest.json
 rm -Rf dist/
 else
 echo "wrong version found! Regenerating..."
-go run registry/*.go "${args[@]}" > registry-manifest.json
+go run $manifest_generator "${args[@]}" > registry-manifest.json
 fi
