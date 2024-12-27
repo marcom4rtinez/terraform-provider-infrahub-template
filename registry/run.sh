724db4b1dd8f75cab506d0b10185f6d1a4952a6f
Marco Martinez <mmmartinez@bluewin.ch>
2024-12-27 16:44:17 +0100

    testing

--- registry/run.sh
+++ registry/run.sh
@@ -10,6 +10,7 @@ search_string="\"version\": \"$2\","
 if grep -q "$search_string" registry-manifest.json; then
 echo "already up2date! Adding hashes now..."
 cat dist/*SHA256SUMS | go run registry/*.go --hashes --manifest registry-manifest.json
+rm -Rf dist/
 else
 echo "wrong version found! Regenerating..."
 go run registry/*.go "${args[@]}" > registry-manifest.json
